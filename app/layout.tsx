11af8405fe2007dc02b2d084ff285887d22f6398
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2025-06-09 13:01:22 +0200

    fix: resolve mono font loading issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import './globals.css'
+import { JetBrains_Mono } from 'next/font/google';
+
+
+const JetBrainsMonoFont = JetBrains_Mono({
+  display: 'swap',
+  variable: '--font-monospace',
+  subsets: ['latin'],
+});
+
 
 export const metadata: Metadata = {
   title: 'Vivekananda Reddy - Portfolio',
@@ -15,11 +24,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
+      <body className={`min-h-screen flex flex-col ${JetBrainsMonoFont.variable}`}>
         {/* Navbar */}
         <header className="pt-8 md:pt-16 md:w-2/3 pb-16 px-4 md:px-0 flex justify-between max-w-7xl mx-auto w-full sm:w-full lg:w-1/2 xl:w-2/5 2xl:w-1/4">
           <Link href="/">
-            <h1 className="text-base font-mono font-semibold text-black">vrg.</h1>
+            <h1 className="text-base font-semibold text-black">vrg.</h1>
           </Link>
           <nav className="flex gap-4">
             <Link
@@ -49,7 +58,7 @@ export default function RootLayout({
         </main>
 
         {/* Footer */}
-        <footer className="flex justify-between mx-auto  bg-[#f9fafd]  px-6 py-6 md:w-1/2 max-w-7xl w-full sm:w-full lg:w-1/2 xl:w-2/5 2xl:w-1/4">
+        <footer className="flex justify-between mx-auto  bg-[#f9fafd]  px-6 py-6 md:w-1/2 max-w-7xl w-full sm:w-full lg:w-1/2 xl:w-2/5 2xl:w-1/4 font-mono">
 
           <div className="max-w-7xl mx-auto">
             {/* Horizontal Line */}
