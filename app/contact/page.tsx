0539264b19f3cef10a8a6a2de772c202f05dce34
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2025-06-09 17:32:59 +0200

    fix: email overlapping with twitter

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 import { Title } from "../components/title";
 import Link from "next/link";
+
 const contact: {
   method: string;
   link: string;
@@ -26,30 +27,33 @@ const contact: {
     label: 'in/vivekananda-reddy',
   },
 ];
+
 export default function Contact() {
   return (
-    
     <main className="px-4">
       <h1 className="mx-auto text-left font-medium text-4xl mb-4 text-slate-950 md:w-1/2 lg:w-1/2 xl:w-2/5 2xl:w-1/4">
         Contact
         <div className="h-px bg-slate-300 my-6" />
         <span className="block text-slate-900 font-light text-xl">
-          If you’d like to get in touch, you can reach me using the following methods. 
+          If you'd like to get in touch, you can reach me using the following methods. 
         </span>
       </h1>
       <section>
-        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto md:w-1/2 lg:w-1/2 xl:w-2/5 2xl:w-1/4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto md:w-1/2 lg:w-1/2 xl:w-2/5 2xl:w-1/4 gap-4 md:gap-6">
           {contact.map((contactMethod) => {
             return (
-              <div className="flex flex-col" key={contactMethod.method}>
+              <div className="flex flex-col min-w-0" key={contactMethod.method}>
                 <Title as="h2" variant="tertiary">
                   {contactMethod.method}
                 </Title>
-                <Link href={contactMethod.link} className="text-slate-700 font-light underline">
+                <Link 
+                  href={contactMethod.link} 
+                  className="text-slate-700 font-light underline break-all sm:break-words overflow-wrap-anywhere"
+                >
                   {contactMethod.label}
                 </Link>
-                </div>
-                );
+              </div>
+            );
           })}
         </div>
       </section>
